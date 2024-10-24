f2fb3b80cefadc2cad8870c31bb46fc9308879ef
Alex The JPEG <alexthejpeg@fastmail.com>
2024-10-24 14:59:16 -0400

    Create socials footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind(), mdx()]
+  integrations: [preact(), tailwind(), mdx(), icon()]
 });
