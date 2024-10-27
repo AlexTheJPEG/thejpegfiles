380738a293a5204278e8dc52098d48e4473cb1d3
Alex The JPEG <alexthejpeg@fastmail.com>
2024-10-26 23:15:01 -0400

    Setup blog and first post

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			margin: {
+				"paragraph-spacing": "15rem",
+			}
+		},
 	},
 	plugins: [],
 }
