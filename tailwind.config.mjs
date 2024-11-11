b22448a80f4b39f533c6c4a3478c2582459da192
Alex The JPEG <alexthejpeg@fastmail.com>
2024-11-11 18:05:53 -0500

    Use `<br />`s instead of using `<p>`s with margins

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: "class",
 	theme: {
-		extend: {
-			margin: {
-				"paragraph-spacing": "15rem",
-			}
-		},
+		extend: {}
 	},
 	plugins: [],
 }
